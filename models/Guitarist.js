a2afdf18faf8dd58389a07aad1ae939358346a4f
Mike Nezzer <57691487+mnezz1131@users.noreply.github.com>
2020-03-21 12:03:10 -0500

    Database done, Api routes done

---
+++ models/Guitarist.js
@@ -0,0 +1,9 @@
+module.exports = function Guitarist (sequelize, DataTypes) {
+  var Guitarist   = sequelize.define("Guitarist", {
+      position: DataTypes.INTEGER,
+      guitarist: DataTypes.STRING,
+      genre: DataTypes.STRING,
+      band: DataTypes.STRING
+  });
+  return Guitarist;
+};
